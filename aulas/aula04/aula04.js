82d622a345a48b1448e51e8cb542641adfdba8d1
estaeldev <estael.dev@gmail.com>
2022-12-15 21:02:49 -0300

    Criado pasta e arquivo aula04

---
+++ aulas/aula04/aula04.js
@@ -0,0 +1,90 @@
+
+// Operadores Aritméticos
+const aritmetico1 = 10 + 5; // 15
+const aritmetico2 = 10 - 5; // 5
+const aritmetico3 = 10 * 5; // 50
+const aritmetico4 = 10 / 5; // 2
+
+const aritmetico5 = 12 % 5;
+console.log(aritmetico5);
+
+// Incremento
+let x = 10;
+console.log(x++);
+
+// Decremento
+let y = 10;
+console.log(--y);
+
+// Exponenciação
+let z = 10;
+console.log(z**2);
+
+// Operadores de String
+
+const nome = "Sergio"
+console.log(nome + "Resende");
+console.log(nome);
+
+// Operadores de atribuição
+let preco = 100;
+console.log(preco += 5);
+console.log(preco -= 10);
+console.log(preco *= 2);
+console.log(preco /= 2);
+console.log(preco %= 4);
+console.log(preco ** 3);
+
+// Operadores de Comparação
+console.log(10 == 10);
+console.log(8 != 10);
+console.log(8 == '8');
+console.log(8 === '8');
+console.log(15 !== '15');
+console.log(100 > 98);
+console.log(100 < 98);
+console.log(100 >= 100);
+console.log(200 <= 199);
+
+// Operadores Logicos
+const notebook = true;
+const dinheiro = true;
+
+// AND
+console.log("Posso comprar o notbook: ", notebook && dinheiro);
+
+// OR
+console.log("Posso comprar o notbook 2: ", notebook || dinheiro);
+
+// NOT
+console.log("A loja está vendendo notebook: ", !notebook);
+
+
+// Operadores Condicionais (ternário)
+const idadeAna = 17;
+console.log(idadeAna >= 18 ? "Ela pode comprar a bedida" : "Proibida de comprar a bebida!");
+
+// Falsy e Trupy
+
+// null = false
+// undefined = false
+// 0 = false
+// NaN = false
+// "" =  false
+
+// "qualquer valor" = true
+// {} = true
+// [] = true
+// 156 = true
+// 12.655 = true
+// -97 = true
+
+ 
+
+
+
+
+
+
+
+
