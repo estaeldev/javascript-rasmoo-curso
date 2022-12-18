dc2219971952c88d40d1dc6f66c2a2cb592c87c7
estaeldev <estael.dev@gmail.com>
2022-12-18 18:13:50 -0300

    Modificando o arquivo aula04

--- aulas/aula04/aula04.js
+++ aulas/aula04/aula04.js
@@ -79,9 +79,121 @@ console.log(idadeAna >= 18 ? "Ela pode comprar a bedida" : "Proibida de comprar 
 // 12.655 = true
 // -97 = true
 
- 
+// Estruturas condicionais e controle de fluxo
+
+// If...else
+const media = 8;
+
+if (media >= 8) {
+    console.log('Aprovado!!!!');
+} else if (media > 5 && media < 8) {
+    console.log('Em recuperação!!!!');
+} else {
+    console.log('Reprovado!');
+}
+
+// Switch
+const situacao = 'APROVADO';
+
+switch (situacao) {
+    case 'APROVADO': {
+        console.log('Logado');
+        break;
+    }
+    case 'EM_ANALISE':
+    case 'PENDENTE': {
+        console.log('Aguardando aprovação.');
+        break;
+    }
+    case 'REPROVADO': {
+        console.log('Tente novamente daqui a 90 dias.');
+        break;
+    }
+    default: {
+        console.log('Ixi!! Não sei que fruta é essa.');
+    }
+}
+
+// Try...catch
+const aluno = undefined;
+
+try {
+    if (!aluno) {
+        throw new SyntaxError('Erro interno. Por favor tente novamente mais tarde.'); s
+    }
+
+    if (!aluno.nome || !aluno.idade) {
+        throw new TypeError('Usuário não preencheu todas as informações obrigatórias.');
+    }
+
+    console.log('Aluno salvo com sucesso!');
+    console.log('Aluno: ', aluno);
+} catch (error) {
+    if (error instanceof TypeError) {
+        console.log(`Erro ocorrou do lado do cliente: ${error.message}`);
+    }
+
+    if (error instanceof SyntaxError) {
+        console.log(`Programador fez besteira: ${error.message}`);
+    }
+
+} finally {
+    console.log('Sempre serei executado!');
+}
 
 
+// Estruturas de repetição
+const paises = ['Brasil', 'Argentina', 'Alemanha', 'Italia'];
+paises.forEach((pais) => {
+    console.log(pais);
+});
+
+console.log('------------------------------------');
+
+// For
+for (let i = 0; i < paises.length; i++) {
+    console.log(paises[i]);
+}
+
+console.log('------------------------------------');
+
+// For of
+for (let pais of paises) {
+    console.log(pais);
+}
+
+console.log('------------------------------------');
+
+function Cliente(nome, email, idade) {
+    this.nome = nome;
+    this.email = email;
+    this.idade = idade;
+}
+
+const clientePedro = new Cliente('Pedro', 'pedro@gmail.com', 30);
+
+// For in
+for (let prop in clientePedro) {
+    console.log(prop, clientePedro[prop]);
+}
+
+console.log('------------------------------------');
+
+// While
+let count = 0;
+
+while (count < 10) {
+    console.log(count);
+    count++;
+}
+
+console.log('------------------------------------');
+
+// Do while
+do {
+    console.log('Opa!');
+    count++;
+} while (count < 20); 
 
 
 
