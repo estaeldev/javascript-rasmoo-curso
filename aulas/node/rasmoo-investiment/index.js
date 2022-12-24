b9194ea2143e9d9327e70993357b1372e0c50bf4
estaeldev <estael.dev@gmail.com>
2022-12-24 13:58:15 -0300

    Conteudo desenvolvido na aula05

--- aulas/node/rasmoo-investiment/index.js
+++ aulas/node/rasmoo-investiment/index.js
@@ -1,4 +1,15 @@
-console.log("Hello World!!");
+const fastify = require('fastify')({
+    logger: true
+});
 
+fastify.get('/teste', function (request, reply) {
+    reply.send({ message: 'Hello World!' })
+});
 
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+});
 
