3100443ec5532905d8d3d5f24dd9471f821c6c83
estaeldev <estael.dev@gmail.com>
2022-12-25 17:21:43 -0300

    Modificação do arquivo index.js

--- aulas/node/rasmoo-investiment/index.js
+++ aulas/node/rasmoo-investiment/index.js
@@ -1,9 +1,33 @@
+class Investment {
+    name;
+    minValue;
+    tax;
+    time;
+
+    constructor(name, minValue, tax, time){
+        this.name = name;
+        this.minValue = minValue;
+        this.tax = tax;
+        this.time = time;
+    }
+
+}
+
+const investiments = [
+    new Investment("CDB - Pós-Fixado", 1000, 103.75, 30), 
+    new Investment("CDB - Pós-Fixado", 1000, 104.25, 60),
+    new Investment("CDB - Pós-Fixado", 5000, 112, 180),
+    new Investment("Tesouro Prefixado 2025", 783.33, 13.10, 1080),
+    new Investment("Tesouro Selic 2025", 11868, 0.1, 1080)
+];
+
+
 const fastify = require('fastify')({
     logger: true
 });
 
-fastify.get('/teste', function (request, reply) {
-    reply.send({ message: 'Hello World!' })
+fastify.get('/investimentos', function (request, reply) {
+    reply.send(investiments);
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
@@ -12,4 +36,6 @@ fastify.listen({ port: 3000 }, function (err, address) {
       process.exit(1)
     }
 });
+
+
 
